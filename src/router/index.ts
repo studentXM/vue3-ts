8cf8848fdad9cfa87df19fdfa6fb22f8bac6ad79
vue后台管理系统 <2953282238@qq.com>
2022-09-26 15:42:13 +0800

    feat(router/views): 添加router和views

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: () => import('../views/login/login.vue')
+  }
+]
+
+const router = createRouter({
+  routes,
+  history: createWebHashHistory()
+})
+
+export default router
