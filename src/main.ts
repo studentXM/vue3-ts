f63fa22723490c7989ab2c29b7a5fa0e536b9edb
xm <2953282238@qq.com>
2022-09-29 02:00:50 +0800

    feat(axios单个拦截): axios单个拦截 介绍

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,12 @@ HYRequest.request({
   url: '/home/multidata',
   method: 'GET',
   interceptors: {
+    // 请求拦截 在请求之前可以对 config进行设置 比如设置一些请求头什么的 这个config就是上面的几个属性
     requestInterceptors: (config) => {
-      console.log('接口拦截')
+      console.log(config)
+      console.log('-------------- 单个请求拦截')
       return config
     }
   },
-  showLoading: false
+  showLoading: true
 })
